bcdcd1f92f2237fb14a785d69a3f41e2a7c4a363
hammad <hammadafroz007@gmail.com>
2024-04-17 09:14:48 +0530

    Solved problem 1

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const findCarById = require('../problem1');
+
+// const inventory = [/* Your inventory array here */];
+// const carId = 33;
+
+// const result = findCarById(inventory, carId);
+// console.log(`Car ${carId} is a ${result.car_year} ${result.car_make} ${result.car_model}`);
